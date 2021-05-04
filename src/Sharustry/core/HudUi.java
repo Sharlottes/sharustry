d8d64b78099b4d6b9b22dbe49642a7e502c7160c
sharlottes <60801210+sharlottes@users.noreply.github.com>
2021-05-04 16:03:22 +0900

    h

--- src/Sharustry/core/HudUi.java
+++ src/Sharustry/core/HudUi.java
@@ -9,20 +9,22 @@ import arc.scene.ui.*;
 import arc.scene.ui.layout.Scl;
 import arc.scene.ui.layout.Stack;
 import arc.scene.ui.layout.Table;
+import arc.struct.Seq;
 import arc.util.Log;
+import arc.util.Scaling;
 import arc.util.Time;
 import mindustry.Vars;
+import mindustry.entities.abilities.ForceFieldAbility;
 import mindustry.entities.abilities.ShieldRegenFieldAbility;
 import mindustry.entities.units.WeaponMount;
-import mindustry.gen.Icon;
-import mindustry.gen.Tex;
-import mindustry.gen.Unit;
+import mindustry.gen.*;
 import mindustry.graphics.Pal;
 import mindustry.type.UnitType;
 import mindustry.type.Weapon;
 import mindustry.ui.Bar;
 import mindustry.ui.Cicon;
 import mindustry.ui.Styles;
+import mindustry.world.blocks.payloads.Payload;
 
 import static arc.Core.scene;
 import static mindustry.Vars.content;
@@ -31,66 +33,83 @@ import static mindustry.Vars.player;
 public class HudUi {
     float scrollPos;
 
+    public Unit getUnit(){
+        Seq<Unit> units = Groups.unit.intersect(Core.input.mouseWorldX(), Core.input.mouseWorldY(), 4, 4);
+        if(units.size <= 0) return player.unit();
+        Unit unit = units.peek();
+        if(unit == null) return player.unit();
+        else return unit;
+    }
+    
     public void reset(Table table){
         table.remove();
         table.reset();
         addTable();
     }
+    
     public void addTable(){
         Vars.ui.hudGroup.addChild(new Table(table -> {
-            Unit unit = player.unit();
+            Unit unit = getUnit();
             table.update(() -> {
-                if(player.unit() != unit) reset(table);
+                if(getUnit() != unit) reset(table);
             });
             table.left();
             table.table(scene.getStyle(Button.ButtonStyle.class).up, t -> {
+                t.table(Tex.underline2, tt -> {
+                    tt.top();
+                    tt.add(new Stack(){{
+                        add(new Table(temp -> {
+                            temp.left();
+                            temp.add(new Image(Icon.defense)).center();
+                        }));
+                        add(new Table(temp -> {
+                            temp.left();
+                            Label label = new Label(() -> (int)(getUnit().type == null ? 0 : getUnit().type.armor) + "");
+                            label.setColor(Pal.surge);
+                            label.setSize(0.6f);
+                            temp.add(label).center().padLeft(getUnit().type == null ? 0 : getUnit().type.armor < 10 ? 8f : 0f);
+                            temp.pack();
+                        }));
+                    }}).growX().left().padRight(3 * 8f);
+                    tt.add(new Label(() ->{
+                        if(getUnit() != null && getUnit().type != null) return "[accent]" + getUnit().type.localizedName + "[]";
+                        return "";
+                    })).center();
+                });
                 t.defaults().size(25 * 8f);
-                t.table(Tex.underline2,  tt -> {
+                t.row();
+                t.table(tt -> {
                     tt.defaults().width(23 * 8f);
                     tt.defaults().height(4f * 8f);
                     tt.top();
 
                     tt.add(new SBar(
-                            () -> Core.bundle.format("shar-stat.health", Mathf.round(player.unit().health,1)),
+                            () -> Core.bundle.format("shar-stat.health", Mathf.round(getUnit().health,1)),
                             () -> Pal.health,
-                            () -> Mathf.clamp(player.unit().health / player.unit().type.health)
+                            () -> Mathf.clamp(getUnit().health / getUnit().type.health)
                     )).growX().left();
                     tt.row();
-                    tt.add(new Stack(){{
-                        add(new Table(temp -> {
-                            temp.defaults().width(23 * 8f);
-                            temp.defaults().height(4f * 8f);
-                            temp.add(new SBar(
-                                    () -> Core.bundle.format("shar-stat.shield", Mathf.round(player.unit().shield,1)),
-                                    () -> Pal.surge,
-                                    () -> {
-                                        ShieldRegenFieldAbility ability = (ShieldRegenFieldAbility)content.units().copy().filter(ut -> ut.abilities.find(abil -> abil instanceof ShieldRegenFieldAbility) != null).sort(ut -> {
-                                            ShieldRegenFieldAbility ability1 = (ShieldRegenFieldAbility)ut.abilities.find(abil -> abil instanceof ShieldRegenFieldAbility);
-                                            return ability1.max;
-                                        }).peek().abilities.find(abil -> (abil instanceof ShieldRegenFieldAbility));
-                                        float max = ability.max;
-                                        return Mathf.clamp(player.unit().shield / max);
-                                    }
-                            )).growX().left();
-                        }));
-                        add(new Table(temp -> {
-                            temp.left();
-                            temp.add(new Image(Icon.defense));
-                            temp.pack();
-                        }));
-                        add(new Table(temp -> {
-                            temp.left();
-                            Label label = new Label(() -> (int)(player.unit().type == null ? 0 : player.unit().type.armor) + "");
-                            label.setColor(Pal.lightishGray);
-                            label.setSize(0.6f);
-                            temp.add(label).padLeft(7.5f);
-                            temp.pack();
-                        }));
-                    }}).growX().left();
+                    tt.add(new SBar(
+                            () -> Core.bundle.format("shar-stat.shield", Mathf.round(getUnit().shield,1)),
+                            () -> Pal.surge,
+                            () -> {
+                                float max1 = ((ShieldRegenFieldAbility)content.units().copy().filter(ut -> ut.abilities.find(abil -> abil instanceof ShieldRegenFieldAbility) != null).sort(ut -> ((ShieldRegenFieldAbility)ut.abilities.find(abil -> abil instanceof ShieldRegenFieldAbility)).max).peek().abilities.find(abil -> abil instanceof ShieldRegenFieldAbility)).max;
+                                float max2 = 0f;
+                                if(getUnit().type.abilities.find(abil -> abil instanceof ForceFieldAbility) != null) max2 = ((ForceFieldAbility) getUnit().type.abilities.find(abil -> abil instanceof ForceFieldAbility)).max;
+                                return Mathf.clamp(getUnit().shield / Math.max(max1, max2));
+                            }
+                    )).growX().left();
+                    tt.row();
+                    if(getUnit() instanceof Payloadc) tt.add(new SBar(
+                            () -> Core.bundle.format("shar-stat.payloadCapacity", Mathf.round(((Payloadc)getUnit()).payloadUsed()), Mathf.round(unit.type().payloadCapacity)),
+                            () -> Pal.items,
+                            () -> Mathf.clamp(((Payloadc)getUnit()).payloadUsed() / unit.type().payloadCapacity)
+                    )).growX().left();
+
                 });
             });
             table.row();
-            UnitType type = player.unit().type;
+            UnitType type = getUnit().type;
             table.left();
             try{
                 table.table(tx -> {
@@ -104,7 +123,7 @@ public class HudUi {
                         for(int r = 0; r < type.weapons.size; r++){
                             final int i = r;
                             Weapon weapon = type.weapons.get(i);
-                            WeaponMount mount = player.unit().mounts[i];
+                            WeaponMount mount = getUnit().mounts[i];
                             TextureRegion region = !weapon.name.equals("") && weapon.outlineRegion.found() ? weapon.outlineRegion : type.icon(Cicon.full);
                             if(type.weapons.size > 1 && i % 3 == 0) tt.row();
                             else if(i % 3 == 0) tt.row();
@@ -113,7 +132,7 @@ public class HudUi {
                                 weapontable.add(new Stack(){{
                                     add(new Table(o -> {
                                         o.left();
-                                        o.add(new Image(region));
+                                        o.image(region).size(60).scaling(Scaling.bounded);
                                     }));
 
                                     add(new Table(h -> {
@@ -144,7 +163,7 @@ public class HudUi {
             table.visibility = () ->
                     Vars.ui.hudfrag.shown && !Vars.ui.minimapfrag.shown()
                             && (!Vars.mobile ||
-                            !(player.unit().isBuilding() || Vars.control.input.block != null || !Vars.control.input.selectRequests.isEmpty()
+                            !(getUnit().isBuilding() || Vars.control.input.block != null || !Vars.control.input.selectRequests.isEmpty()
                                     && !(Vars.control.input.lastSchematic != null && !Vars.control.input.selectRequests.isEmpty())));
         }));
     }
--- src/Sharustry/core/Main.java
+++ src/Sharustry/core/Main.java
@@ -1,48 +1,20 @@
 package Sharustry.core;
 
 import Sharustry.content.STurretMounts;
-import Sharustry.ui.SBar;
 import Sharustry.world.blocks.defense.MountTurretType;
-import arc.Core;
 import arc.Events;
-import arc.graphics.Color;
-import arc.graphics.g2d.TextureRegion;
-import arc.math.Mathf;
-import arc.scene.ui.*;
-import arc.scene.ui.layout.Scl;
-import arc.scene.ui.layout.Stack;
-import arc.scene.ui.layout.Table;
-import arc.util.Log;
-import arc.util.Strings;
-import arc.util.Time;
-import mindustry.Vars;
-import mindustry.content.Items;
-import mindustry.content.UnitTypes;
-import mindustry.entities.abilities.ShieldRegenFieldAbility;
-import mindustry.entities.units.WeaponMount;
 import mindustry.game.EventType.*;
-import mindustry.gen.Icon;
-import mindustry.gen.Tex;
-import mindustry.graphics.Pal;
 import mindustry.mod.Mod;
 
 import Sharustry.content.ModLoader;
-import mindustry.type.*;
-import mindustry.ui.*;
-
-import static arc.Core.scene;
-import static mindustry.Vars.*;
 
 public class Main extends Mod {
     public Main(){
         Events.on(ClientLoadEvent.class, e -> {
-            new HudUi().addTable();
-
             for(MountTurretType mount : STurretMounts.mounttypes) mount.load();
         });
 
         Events.on(WorldLoadEvent.class, e -> {
-            new HudUi().addTable();
         });
     }
 
