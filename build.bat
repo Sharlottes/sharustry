8023a9e8fe09e76911cf65361375a719be865034
Sharlotte <aaa9810321@gmail.com>
2023-10-31 01:26:43 +0900

    chore: set up build

--- build.bat
+++ build.bat
@@ -1,9 +1,9 @@
 @rem put this project path into PATH_FROM
 setlocal
-set PATH_FROM=C:\Users\jun\Documents\GitHub\sharustry
+set PATH_FROM=C:\Users\user\Documents\GitHub\sharustry
 @rem put your mindustry local path into PATH_TO
 setlocal
-set PATH_TO=C:\Users\jun\AppData\Roaming\Mindustry
+set PATH_TO=C:\Users\user\AppData\Roaming\Mindustry
 
-if exist %PATH_TO%\mods\raw-sharustry.jar del %PATH_TO%\mods\raw-sharustry.jar
-xcopy %PATH_FROM%\build\libs\raw-sharustry.jar %PATH_TO%\mods\ /k /y
+if exist %PATH_TO%\mods\SharustryDesktop.jar del %PATH_TO%\mods\SharustryDesktop.jar
+xcopy %PATH_FROM%\build\libs\SharustryDesktop.jar %PATH_TO%\mods\ /k /y
